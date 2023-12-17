8ab0a0a706fcb88a7cd7cd55cff0bda2fee05d18
osparking <jbpark03@gmail.com>
2023-12-17 12:03:59 +0900

    import 수정 - JUnit 5 에 일치

--- src/test/java/space/bum/junit/AppTest.java
+++ src/test/java/space/bum/junit/AppTest.java
@@ -1,8 +1,8 @@
 package space.bum.junit;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
